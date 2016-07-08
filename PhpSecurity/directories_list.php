8db83bb9c275936cb23120f7659af7648b76ff72
Mohamed Ataala <mohamed.ataala@gmail.com>
2016-07-08 03:56:26 +0200

    dirs-list

---
+++ PhpSecurity/directories_list.php
@@ -0,0 +1,8 @@
+<?php
+
+# to prevent shows the directories list for your project in user's browser
+# copy the following command to .htaccess file
+Options -Indexes
+# Hint => working only with apache server
+
+#----------------------------------------------------------------------------
